131226db6d2505af37690fe0b7e6b70a66c72d99
Debi Codriansky <debicps@gmail.com>
2023-04-01 15:35:14 +0300

    link

--- src/Pages/ProjectInfo.js
+++ src/Pages/ProjectInfo.js
@@ -49,8 +49,8 @@ if (isLoading && !currentProject) {
         <h2>About</h2>
         <div className='project_info_column'>
             <p>{currentProject.long_description}</p>
-            {currentProject.web_link=="none"?  <></> : <a href={currentProject.web_link}>Website link</a>}
-            {currentProject.github_link=="none"?  <></> : <a href={currentProject.github_link}>Github link</a>}
+            {currentProject.web_link==="none"?  <></> : <a target="_blank" href={currentProject.web_link}>Website link</a>}
+            {!currentProject.github_link==="none"?  <></> : <a target="_blank"  href={currentProject.github_link}>Github link</a>}
 
       </div>
     </div>
